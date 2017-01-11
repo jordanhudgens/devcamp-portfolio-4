8347665f4608c59afe5a8be178ec159e71d41927
TinCanDan <gilworthen@gmail.comn>
2017-01-10 20:02:50 -0700

    Implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class PortfoliosController < ApplicationController
   def index
-    @portfolio_item = Portfolio.all
+    @portfolio_items = Portfolio.all
   end
 
   def new
@@ -8,31 +8,47 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
-    if @portfolio_item.save
-      redirect_to @portfolio_item
-    else
-      render :new
-    end
-  end
-end
-
-def edit
-  @portfolio_item = Portfolio.find(params[:id])
-end
-
- def update
-  @portfolio_item = Portfolio.find(params[:id])
-
-    respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-
-        format.html { redirect_to @blog, notice: 'The record successfully updated.' }
+  
+      if @portfolio_item.save
+        redirect_to @portfolio_item
       else
-        format.html { render :edit }
+        render :new
       end
-    end
   end
 
+    def show
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def edit
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+      @portfolio_item = Portfolio.find(params[:id])
+
+      if @portfolio_item.update(portfolio_params)
+        redirect_to @portfolio_item
+      else
+        render :edit
+
+      end
+
+    end
+
+    def destroy
+      @portfolio_item = Portfolio.find(params[:id])
+      @portfolio_item.destroy
+      redirect_to portfolios_path
+    end
+
+    private
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image, :main_image)
+    end
+
+end
 
