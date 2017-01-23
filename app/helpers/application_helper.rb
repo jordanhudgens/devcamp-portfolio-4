481c89d269062c94100efa9974fb849176fed107
TinCanDan <gilworthen@gmail.comn>
2017-01-23 12:18:51 -0700

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
      content_tag(:p, greeting, class: "source-greeting")
      end
   end
+
+  def copyright_generator
+    @copyright = WorthenViewTool::Renderer.copyright "Dan Worthen", "All rights reserved"
+  end
 end
