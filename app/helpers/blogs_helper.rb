c52ddc6bdb2c614900de9b52401e58667c44032e
TinCanDan <gilworthen@gmail.comn>
2017-02-03 17:39:14 -0700

    Added cool buttons and other things for blogs

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,12 @@
 module BlogsHelper
+  def blog_status_color blog
+   'background: green' if blog.Published?
+  end
+  def blog_status_class blog
+    if blog.Published?
+      'btn btn-primary btn-green'
+    else
+      'btn btn-primary'
+    end
+  end
 end
