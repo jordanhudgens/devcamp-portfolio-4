b9ae00180bcf59f5ae84fc56eabd4298ead85f36
TinCanDan <gilworthen@gmail.comn>
2017-02-06 08:49:26 -0700

    Blog helper saved

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,7 +1,4 @@
 module BlogsHelper
-  def blog_status_color blog
-   'background: green' if blog.Published?
-  end
   def blog_status_class blog
     if blog.Published?
       'btn btn-primary btn-green'
